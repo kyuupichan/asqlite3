62a0aa8ffabc096f9712fbccb874b03be4a9a541
neil <neil>
2024-02-13 21:40:02 +0000

    Clean up import logic.  Set version to 0.5

--- asqlite3/__init__.py
+++ asqlite3/__init__.py
@@ -25,43 +25,17 @@ if _sys_version_info >= (3, 11):
         SQLITE_LIMIT_VARIABLE_NUMBER, SQLITE_LIMIT_TRIGGER_DEPTH, SQLITE_LIMIT_WORKER_THREADS
     )
 
-if _sys_version_info >= (3, 12):
-    from sqlite3 import (
-        LEGACY_TRANSACTION_CONTROL, SQLITE_DBCONFIG_DEFENSIVE, SQLITE_DBCONFIG_DQS_DDL,
-        SQLITE_DBCONFIG_DQS_DML, SQLITE_DBCONFIG_ENABLE_FKEY,
-        SQLITE_DBCONFIG_ENABLE_FTS3_TOKENIZER, SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION,
-        SQLITE_DBCONFIG_ENABLE_QPSG, SQLITE_DBCONFIG_ENABLE_TRIGGER, SQLITE_DBCONFIG_ENABLE_VIEW,
-        SQLITE_DBCONFIG_LEGACY_ALTER_TABLE, SQLITE_DBCONFIG_LEGACY_FILE_FORMAT,
-        SQLITE_DBCONFIG_NO_CKPT_ON_CLOSE, SQLITE_DBCONFIG_RESET_DATABASE,
-        SQLITE_DBCONFIG_TRIGGER_EQP, SQLITE_DBCONFIG_TRUSTED_SCHEMA,
-        SQLITE_DBCONFIG_WRITABLE_SCHEMA
-    )
-
-_deprecated_names = {
-    'version': ((3, 12), (3, 14)),
-    'version_info': ((3, 12), (3, 14)),
-}
-
 
 def __getattr__(name):
-    versions = _deprecated_names.get(name)
-    if versions:
-        import sqlite3
-        first, last = versions
-        if first <= _sys_version_info < last:
-            return getattr(sqlite3, name)
+    '''Handles deprecated names and conditionally defined names such as SQLITE_DBCONFIG_xxx.'''
+    import sqlite3
 
-    raise AttributeError(f'module asqlite3 has no attribte {name}')
+    return getattr(sqlite3, name)
 
-
-if _sys_version_info < (3, 12):
-    from sqlite3 import (
-        version, version_info
-    )
 
 from .asqlite3 import (
     Cursor, Connection, connect,
 )
 
-_version_str = '0.1'
+_version_str = '0.5'
 _version = tuple(int(part) for part in _version_str.split('.'))
