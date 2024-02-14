fcd8acd5944442ec1c889c08441d2927ebd72112
neil <neil>
2024-02-14 09:13:11 +0000

    Add README.rst and setup.py

---
+++ README.rst
@@ -0,0 +1,30 @@
+.. image:: https://badge.fury.io/py/bitcoinX.svg
+    :target: http://badge.fury.io/py/bitcoinX
+
+===========
+py-asqlite3
+===========
+
+A simple and lightweight async wrapper of everything from the built-in sqlite3.
+
+  :Licence: MIT
+  :Language: Python (>= 3.8)
+  :Author: Neil Booth
+
+
+Documentation
+=============
+
+See `readthedocs <https://py-asqlite3.readthedocs.io/>`_.
+
+
+**Neil Booth**  kyuupichan@pm.me  https://github.com/kyuupichan
+
+
+ChangeLog
+=========
+
+0.5.0
+-----
+
+Close to release state.  Docs and some more testcases needed.
---
+++ setup.py
@@ -0,0 +1,42 @@
+import os
+import re
+import setuptools
+
+
+def find_version(filename):
+    with open(filename) as f:
+        text = f.read()
+    match = re.search(r"^_version_str = '(.*)'$", text, re.MULTILINE)
+    if not match:
+        raise RuntimeError('cannot find version')
+    return match.group(1)
+
+
+tld = os.path.abspath(os.path.dirname(__file__))
+version = find_version(os.path.join(tld, 'asqlite3', '__init__.py'))
+
+
+setuptools.setup(
+    name='py-asqlite3',
+    version=version,
+    scripts=[],
+    python_requires='>=3.8',
+    install_requires=[],
+    packages=['asqlite3'],
+    description='Async wrapper for sqlite3',
+    author='Neil Booth',
+    author_email='kyuupichan@pm.me',
+    license='MIT Licence',
+    url='https://github.com/kyuupichan/asqlite3',
+    long_description='Async wrapper for sqlite3',
+    download_url=('https://github.com/kyuupichan/asqlite3/archive/'
+                  f'{version}.tar.gz'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: AsyncIO',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Database",
+    ],
+)
